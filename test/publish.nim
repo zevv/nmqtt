1bbc306b4b2d625f708f7dfbca7ed4c130467867
ThomasTJdev <ttjarloev@gmail.com>
2020-03-17 07:27:01 +0100

    Clean up retain test

--- test/publish.nim
+++ test/publish.nim
@@ -1,11 +1,10 @@
 
 suite "test suite for publish":
 
-
-  test "publish retain":
+  test "publish retain msg":
     ## Awaiting PR #16
 
-    let (tpc, msg) = tdata("publish retain")
+    let (tpc, msg) = tdata("publish retain msg")
     waitFor ctxMain.publish(tpc, msg, qos=2, retain=true, true)
 
     proc conn() {.async.} =
@@ -13,20 +12,22 @@ suite "test suite for publish":
         msgFound: bool
         timeout: int
 
+      # Start listening slave
       await ctxSlave.start()
-      await sleepAsync(2000)
       proc on_data(topic: string, message: string) =
         echo topic
         if topic == tpc:
-          echo message
           check(message == msg)
           msgFound = true
           return
 
       await ctxMain.subscribe(tpc, 2, on_data)
 
+      # Wait for retained msg is found
       while not msgFound:
         if timeout == 5:
+          # In an ideal world this should take 0sec, but to include
+          # bad connections and latency we wait 5sec.
           check(msgFound == true)
           break
         await sleepAsync(1000)
@@ -35,9 +36,8 @@ suite "test suite for publish":
       await ctxSlave.close()
       ctxSlave.state = Disabled
 
-
-
     waitFor conn()
+
 
   #[
   test "publish multiple message fast":
