e16786ea73541c4a230d8b756cad537ea42ec97a
ThomasTJdev <ttjarloev@gmail.com>
2020-03-22 19:17:28 +0100

    Check packages in subscribe test

--- test/subscribe.nim
+++ test/subscribe.nim
@@ -12,6 +12,12 @@ suite "test suite for subscribe":
       await ctxListen.subscribe(tpc, 0, on_data)
       await sleepAsync 500
       await ctxMain.publish(tpc, msg, 0)
+      await sleepAsync 500
+
+      check(testDmp[0][0] == "tx> Subscribe(02):")
+      check(testDmp[1][0] == "rx> SubAck(00):" and testDmp[1][1] == "00 01 00 ")
+      check(testDmp[2][0] == "tx> Publish(00):")
+      check(testDmp[3][0] == "rx> Publish(00):")
 
     waitFor conn()
 
@@ -29,6 +35,13 @@ suite "test suite for subscribe":
       await ctxMain.publish(tpc, msg, 1)
       await sleepAsync 500
 
+      check(testDmp[0][0] == "tx> Subscribe(02):")
+      check(testDmp[1][0] == "rx> SubAck(00):" and testDmp[1][1] == "00 01 01 ")
+      check(testDmp[2][0] == "tx> Publish(02):")
+      check(testDmp[3][0] == "rx> PubAck(00):" and testDmp[3][1] == "00 01 ")
+      check(testDmp[4][0] == "rx> Publish(02):")
+      check(testDmp[5][0] == "tx> PubAck(02):" and testDmp[5][1] == "00 01 ")
+
     waitFor conn()
 
   
@@ -44,6 +57,16 @@ suite "test suite for subscribe":
       await sleepAsync 500
       await ctxMain.publish(tpc, msg, 2)
       await sleepAsync 500
+
+      check(testDmp[0][0] == "tx> Subscribe(02):")
+      check(testDmp[1][0] == "rx> SubAck(00):" and testDmp[1][1] == "00 01 02 ")
+      check(testDmp[2][0] == "tx> Publish(04):")
+      check(testDmp[3][0] == "rx> PubRec(00):" and testDmp[3][1] == "00 01 ")
+      check(testDmp[4][0] == "tx> PubRel(02):" and testDmp[4][1] == "00 01 ")
+      check(testDmp[5][0] == "rx> PubComp(00):" and testDmp[5][1] == "00 01 ")
+      check(testDmp[6][0] == "rx> Publish(04):")
+      check(testDmp[7][0] == "tx> PubRel(02):" and testDmp[7][1] == "00 01 ")
+      check(testDmp[8][0] == "rx> PubComp(00):" and testDmp[8][1] == "00 01 ")
 
     waitFor conn()
 
