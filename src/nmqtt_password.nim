c4cb914d0e3a920641055d4237cab8f71743b869
ThomasTJdev <ttjarloev@gmail.com>
2020-04-19 19:20:21 +0200

    Update CLI output from nmqtt_password

--- src/nmqtt_password.nim
+++ src/nmqtt_password.nim
@@ -6,6 +6,8 @@ from os import fileExists
 from strutils import format, split
 from terminal import readPasswordFromStdin
 
+import utils/version
+
 
 proc addUserToFile(filename, username, password: string) =
   ## Adds a new user
@@ -53,21 +55,18 @@ proc deleteUserToFile(filename, username: string) =
 
 proc nmqttPassword(adduser=false, batch=false, deluser=false, args: seq[string]) =
   ## Main handler
+  echo "Running nmqtt_password v" & nmqttVersion
 
   if args.len() == 0:
     echo "Error, missing parameters. Run again with --help."
     quit()
 
-  echo """
-nmqtt_broker v$1
-
-Editing password_file
- - $2
-""".format("x.x.x", args[0])
+  echo "\nEditing password_file\n - " & args[0] & "\n"
 
   if adduser:
     if args.len() != 2:
-      echo "Please provide a path to the password file and a username.\n"
+      echo "Please provide a path to the password file and a username.\n" &
+           " - nmqtt_password -a {password_file.conf} {username}"
       quit()
     let
       prompt   = "Username: $1\nPassword: ".format(args[1])
@@ -76,13 +75,15 @@ Editing password_file
 
   elif batch:
     if args.len() != 3:
-      echo "Please provide a path to the password file, a username and password.\n"
+      echo "Please provide a path to the password file, a username and password.\n" &
+           " - nmqtt_password -b {password_file.conf} {username} {password}"
       quit()
     addUserToFile(args[0], args[1], args[2])
 
   elif deluser:
     if args.len() != 2:
-      echo "Please provide a path to the password file and a username.\n"
+      echo "Please provide a path to the password file and a username.\n" &
+           " - nmqtt_password -d {password_file.conf} {username}"
       quit()
     deleteUserToFile(args[0], args[1])
 
@@ -92,7 +93,10 @@ Editing password_file
 
 
 when isMainModule:
-  const topLvlUse = """${doc}
+  const topLvlUse = """nmqtt_password is a user and password manager for nmqtt
+nmqtt_password is based upon nmqtt version """ & nmqttVersion & """
+
+
 USAGE
   $command -a {password_file.conf} {username}
   $command -b {password_file.conf} {username} {password}
@@ -107,12 +111,11 @@ $options
   clCfg.hTabCols = @[clOptKeys, clDescrip]
 
   dispatchGen(nmqttPassword,
-          doc="Add users and passwords to nmqtt's password file.",
           cmdName="nmqtt_password",
           help={
             "adduser":  "add a new user to the password file.",
             "batch":    "run in batch mode to allow passing passwords on the command line.",
-            "deluser":  "delete a user form the password file.",
+            "deluser":  "delete a user from the password file.",
           },
           short={
             "help": '?',
